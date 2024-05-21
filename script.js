816111b4e7af6a84f00ecfdcec0944a86efed413
Marino6 <drujmmmersjsaldkflkjafsd@outlook.com>
2024-05-21 19:46:39 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -4,3 +4,20 @@ function colorChange()
             var colorValue = colorInput.value;
      body.style.backgroundColor = "#" + colorValue;
 }
+
+const moveableButton = document.getElementById('moveableButton');
+
+moveableButton.addEventListener('click', (event) => {
+    moveableButton.style.transition = 'none';
+    moveButton(event);
+});
+
+document.addEventListener('mousemove', moveButton);
+
+function moveButton(event) {
+    const mouseX = event.clientX;
+    const mouseY = event.clientY;
+    moveableButton.style.left = mouseX + 'px';
+    moveableButton.style.top = mouseY + 'px';
+}
+
