c971aa13a315a147ff20ec9364da8d74b0892b60
Marino6 <drujmmmersjsaldkflkjafsd@outlook.com>
2024-05-22 20:54:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,24 +6,26 @@ function colorChange() {
 }
 
 console.log("great");
-
 function garbage(event) {
-    const mouseX = event.clientX;
-    const mouseY = event.clientY;
-    
-    // Get the moveableButton element
-    const moveableButton = document.getElementById('moveableButton');
-    
-    // Get the current top position of the button relative to the viewport
-    const buttonTop = moveableButton.getBoundingClientRect().top;
-    
-    // Check if the mouse position is different from the current top position of the button
-    if (mouseY < buttonTop) {
-        // Update the top position of the button to move it downward
-        moveableButton.style.top = (buttonTop + 3) + "px"; // Increment the position by 3 pixels
-    } else {
-        // Update the top position of the button to move it upward
-        moveableButton.style.top = (buttonTop - 3) + "px"; // Decrement the position by 3 pixels
+    // Check if the event is a mouse move event
+    if (event.type === 'mousemove') {
+        const mouseX = event.clientX;
+        const mouseY = event.clientY;
+        
+        // Get the moveableButton element
+        const moveableButton = document.getElementById('moveableButton');
+        
+        // Get the current top position of the button relative to the viewport
+        const buttonTop = moveableButton.getBoundingClientRect().top;
+        
+        // Check if the mouse position is different from the current top position of the button
+        if (mouseY < buttonTop) {
+            // Update the top position of the button to move it downward
+            moveableButton.style.top = (buttonTop + 3) + "px"; // Increment the position by 3 pixels
+        } else {
+            // Update the top position of the button to move it upward
+            moveableButton.style.top = (buttonTop - 3) + "px"; // Decrement the position by 3 pixels
+        }
     }
 }
 
