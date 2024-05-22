8227b457e3d752dd33523484a8484a4eb32aee2d
Marino6 <drujmmmersjsaldkflkjafsd@outlook.com>
2024-05-22 20:23:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+
+    var colorValue = colorInput.value;
+function colorChange() {
+    var colorInput = document.getElementById('color');
+    document.body.style.backgroundColor = "#" + colorValue;
+}
+
+if (colorValue == 69420)
+{
+console.log("what the fuck");
+}
+
+    let mouseX = event.clientX;
+    let mouseY = event.clientY;
+    
+if (mouseX != moveableButton.style.top)
+{
+moveableButton.style.top = (0.1 + "px" % 1000) ;
+}
+
+
