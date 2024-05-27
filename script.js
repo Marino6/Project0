1e1703c36fadcc9650b39a4c9d0f957fa1363c50
Marino6 <drujmmmersjsaldkflkjafsd@outlook.com>
2024-05-27 23:39:13 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
 let that = 0; 
 
-function Search() {
-    console.log("clicked");
+function Search() 
+{
+    var searchText = document.getElementById("searchBar").value.toLowerCase();
+    var content = document.getElementById("letter").innerHTML;
+  
+    // Create a regular expression pattern to match the search text
+    var pattern = new RegExp(searchText, 'gi');
+  
+    // Replace the matched text with italicized text
+    var replacedContent = content.replace(pattern, function(match) {
+      return '<i>' + match + '</i>';
+    });
+  
+    // Update the HTML content with the replaced text
+    document.getElementById("letter").innerHTML = replacedContent;
 }
 
-function change() {
+function change() 
+{
     let fun = document.getElementById('search');
     let fan = document.getElementById('more');
     let fin = document.getElementById('setting');
@@ -13,6 +27,17 @@ function change() {
     fan.style.background = `linear-gradient(${(what + 30) % 360}deg, #ff7e5f, #122cbe)`;
     fin.style.background = `linear-gradient(${(what + 60) % 360}deg, #ff7e5f, #122cbe)`;
     that++;
+}
+function pop()
+{
+    var sidebar = document.getElementById("sidebar");
+    sidebar.classList.toggle("active");
+}
+function changer()
+{
+var input = document.getElementById("inputChange");
+var value = input.value;
+document.body.style.backgroundColor = value;
 }
 
 setInterval(change, 10);
