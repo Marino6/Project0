2dee1e539ac68726e7db6704c98051a19312e094
Marino6 <drujmmmersjsaldkflkjafsd@outlook.com>
2024-05-27 17:04:43 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
+let that = 0; 
+
 function Search() {
- console.log("clicked");
+    console.log("clicked");
 }
- 
-var that = 0;
+
 function change() {
-    var fun = document.getElementById('search');
-    var fan = document.getElementById('more');
-   
+    let fun = document.getElementById('search');
+    let fan = document.getElementById('more');
+    let fin = document.getElementById('setting');
     var what = (that + 1) % 360;
     fun.style.background = `linear-gradient(${what}deg, #ff7e5f, #122cbe)`;
-    fan.style.background = `linear-gradient(${what}deg, #ff7e5f, #122cbe)`;
+    fan.style.background = `linear-gradient(${(what + 30) % 360}deg, #ff7e5f, #122cbe)`;
+    fin.style.background = `linear-gradient(${(what + 60) % 360}deg, #ff7e5f, #122cbe)`;
     that++;
 }
-setInterval(change, 100);
+
+setInterval(change, 10);
