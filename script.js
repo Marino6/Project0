7b1473b2e8259beb78afaf449ad1fec71702338e
Marino6 <drujmmmersjsaldkflkjafsd@outlook.com>
2024-05-27 14:33:14 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
-
-function colorChange() {
-    var colorInput = document.getElementById('color');
-    var colorValue = colorInput.value;
-    document.body.style.backgroundColor = "#" + colorValue;
+function Search() {
+ console.log("clicked");
 }
-
+ 
+var that = 0;
+function change() {
+    var fun = document.getElementById('search');
+    var fan = document.getElementById('more');
+   
+    var what = (that + 1) % 360;
+    fun.style.background = `linear-gradient(${what}deg, #ff7e5f, #122cbe)`;
+    fan.style.background = `linear-gradient(${what}deg, #ff7e5f, #122cbe)`;
+    that++;
+}
+setInterval(change, 100);
