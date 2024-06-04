6d5a4e542e808081250d1b5cd0a1aa230e60d719
Marino6 <drujmmmersjsaldkflkjafsd@outlook.com>
2024-06-04 13:10:32 +0530

    I replaced Typescript with Javascript

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var that = 0;
+that = 0;
 function Search() {
     var searchBar = document.getElementById("searchBar");
     var searchText = searchBar.value.toLowerCase();
@@ -15,11 +15,9 @@ function Search() {
 function change() {
     var fun = document.getElementById('search');
     var fan = document.getElementById('more');
-    var fin = document.getElementById('setting');
     var what = (that + 1) % 360;
-    fun.style.background = "linear-gradient(".concat(what, "deg, #ff7e5f, #122cbe)");
-    fan.style.background = "linear-gradient(".concat((what + 30) % 360, "deg, #ff7e5f, #122cbe)");
-    fin.style.background = "linear-gradient(".concat((what + 60) % 360, "deg, #ff7e5f, #122cbe)");
+    fun.style.background = `linear-gradient(${what}deg, #ff7e5f, #122cbe)`;
+    fan.style.background = `linear-gradient(${what}deg, #ff7e5f, #122cbe)`;
     that++;
 }
 function pop() {
@@ -31,4 +29,4 @@ function changer() {
     var value = input.value;
     document.body.style.backgroundColor = value;
 }
-setInterval(change, 10);
+setInterval(change, 1);
